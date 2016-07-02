a986d95cc5dd9413bb12079a6fdbbdf2a864a948
Rohde Fischer <rohdef@rohdef.dk>
2016-07-02 16:12:43 +0200

    Added initial dummy code and a working karma setup

---
+++ app/dummy.js
@@ -0,0 +1,3 @@
+var dummy = function(a, b) {
+  return a+b;
+};
---
+++ unit-tests/dummy.test.js
@@ -0,0 +1,6 @@
+describe("This is just testing the dummy js file", function() {
+  it("Does add two numbers", function() {
+    // Foo bar
+    expect(dummy(2, 3)).toEqual(5);
+  });
+});
